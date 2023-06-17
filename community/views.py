aa5eb83f2309f6273605b87a6995de5e16348606
Joao4569 <johnnyrod4569@gmail.com>
2023-06-17 16:51:30 +0000

    feat: Setup views.py and urls.py, create community.html for community app

--- community/views.py
+++ community/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# handle request to view community.html - Johnny
+def community(request):
+    return render(request, 'community.html')
