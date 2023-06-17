4715d27fbd93380a61376c5df8fd4b4de33b73dc
Joao4569 <johnnyrod4569@gmail.com>
2023-06-17 20:42:19 +0000

    fix: Fix bug with community views

--- community/views.py
+++ community/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 # handle request to view community.html - Johnny
 def community(request):
-    return render(request, 'community.html')
+    template = 'community/community.html'
+    return render(request, template)
