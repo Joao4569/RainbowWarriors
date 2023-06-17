88df060ce5998dde13ddd85ea88bfcf0466f5829
Joao4569 <johnnyrod4569@gmail.com>
2023-06-17 16:26:39 +0000

    chore: Expose community app models to admin site via admin.py file

--- community/admin.py
+++ community/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 # Import UserProfile model - Johnny
 from .models import UserProfile
+# Import Event model - Johnny
+from .models import Event
+# Import Comment model - Johnny
+from .models import Comment
 
 # Register your models here.
 
-# Expose model to admin panel - Johnny
+# Expose models to admin panel - Johnny
 admin.site.register(UserProfile)
+admin.site.register(Event)
+admin.site.register(Comment)
