686fe4ba096c40c72846b0427a41977e90dc9b8b
Joao4569 <johnnyrod4569@gmail.com>
2023-06-17 20:26:52 +0000

    chore: Connecting community app url's to project url's

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('community/', views.community, name='community'),
+    path('', views.community, name='community'),
 ]
--- rainbowwarriors/urls.py
+++ rainbowwarriors/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+     # Path to Community app URL's -  Johnny
+    path('community/', include('community.urls')),
 ]
