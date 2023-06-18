bd8ce7ce56e72aaeac97f72898656a7c08325272
Joao4569 <johnnyrod4569@gmail.com>
2023-06-18 14:37:25 +0000

    fix: Attempted fix of fields on event model

--- community/models.py
+++ community/models.py
@@ -108,9 +108,11 @@ class Event(models.Model):
     # Compulsory field
     address = models.CharField(max_length=80, null=False)
 
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True,
+                                   blank=True)
 
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True,
+                                    blank=True)
 
 
 class Comment(models.Model):
