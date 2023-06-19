cab55fce3dd5e1245101e50344365e8b723f9410
devancadman@outlook.com <devancadman@outlook.com>
2023-06-19 02:07:42 +0100

    Added JS for Back to Top

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,41 @@
 tailwind.config = {
-    darkMode: 'class',
-  }
-    
-    
-  // grab everything we need
-  const btn = document.querySelector("button.mobile-menu-button");
-  const menu = document.querySelector(".mobile-menu");
-  
-  // add event listeners
-  btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-  });
-  
-  function darkModeListener() {
-    document.querySelector("html").classList.toggle("dark");
-  }
-  
-  document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
+  darkMode: 'class',
+}
   
   
+// grab everything we need
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// add event listeners
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// Dark mode
+function darkModeListener() {
+  document.querySelector("html").classList.toggle("dark");
+}
+document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
+
+// Back to top button
+const BackToTop = document.createElement('div');
+BackToTop.classList.add('back-to-top');
+document.body.appendChild(BackToTop);
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 20) {
+        BackToTop.classList.add('show');
+    } else {
+        BackToTop.classList.remove('show');
+    }
+});
+
+document.body.addEventListener('click', function(event) {
+    if (event.target.classList.contains('back-to-top')) {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+});
