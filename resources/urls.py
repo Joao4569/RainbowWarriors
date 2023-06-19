568999c33cc55a022945b29483054de4fc5ee983
Alfredo Morieri <fred.morieri@gmail.com>
2023-06-19 12:55:03 +0100

    add stories template

--- resources/urls.py
+++ resources/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (Resources, Update_Resource,
-                    Add_Resource, Delete_Resource)
+                    Add_Resource, Delete_Resource,
+                    Stories)
 
 urlpatterns = [
     path('', Resources.as_view(), name='resources'),
     path('add/', Add_Resource.as_view(), name='add_resource'),
-    path('update/<int:pk>', Update_Resource.as_view(), name='update_resource'),
-    path("delete/<int:pk>/", Delete_Resource.as_view(), name="delete_resource"),
+    path('update/<int:pk>/', Update_Resource.as_view(), name='update_resource'),
+    path('delete/<int:pk>/', Delete_Resource.as_view(), name="delete_resource"),
+    path('stories/', Stories.as_view(), name="stories"),
 ]
